9d574024484852c26582e5e316b7c93dccf22dcc
Pat <patbelcher@yahoo.com>
2023-06-07 20:47:49 -0400

    send soem function files from june 7

---
+++ coneCalculator.js
@@ -0,0 +1,12 @@
+//Cone Calculator
+//Two parameters: radius and height
+//Return the volume of a cone with these conditions
+//Formula: https://www.google.com/search?q=volume+of+a+cone
+
+function coneCalculator(radius, height) {
+    const volume = 1/3 * height * 3.14 * Math.pow(radius, 2);
+    return volume;
+}
+
+const finalVolume = coneCalculator(4, 5);
+console.log("The volume of the cone is " + finalVolume); 
